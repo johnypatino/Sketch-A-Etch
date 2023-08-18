0ee1dafeb2cb80632b2710e56abfed222e968c66
Jonathan <johnypatino99@gmail.com>
2023-08-17 21:57:30 -0400

    added new grid button functionality

--- script.js
+++ script.js
@@ -16,4 +16,30 @@ for( let i = 0; i < 16; i++ ) {
     }
     grid.appendChild(rows)
 }
+const button = document.querySelector('button');
+button.addEventListener('click', ()=> {
+    let x = parseInt(prompt('Enter new grid (1-100)'));
+
+        while ( grid.firstChild != null) {
+            grid.removeChild(grid.firstChild);
+        }
+    
+    for( let i = 0; i < x; i++ ) {
+        const rows = document.createElement('div');
+        rows.style.display = 'flex';
+        rows.style.flex = '1';
+        rows.style.flexDirection = 'column';
+        for (let j = 0; j < x; j++) {
+        const square = document.createElement('div');
+        square.style.flex = '1';
+        square.style.borderStyle = 'solid'
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'blue';
+        })
+            rows.appendChild(square);
+        }
+        grid.appendChild(rows)
+    }
+
+})
 
